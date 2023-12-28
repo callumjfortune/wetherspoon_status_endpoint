c1d94352f938808351cb9b2264c56ac98210c7e3
callumjfortune <callum.fortune@bbc.co.uk>
2023-12-28 15:23:46 +0000

    tidy + install react-icons

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App(timeData) {
 
       <div className=''>
 
-        <h1 className='mb-2 text-[2em] font-[500]'>The George Inn</h1>
-        <h2 className='mb-8 text-[1.4em] text-gray-600'>is currently achieving the following times</h2>
+        <h1 className='mb-2 text-[3em] font-[500]'>The George Inn</h1>
+        <h2 className='mb-8 text-[2em] text-gray-600'>is currently achieving the following times</h2>
 
         <div className='flex items-center justify-around'>
           <div>
-            <h1 className='text-[1.2em] mb-2'>Food</h1>
-            <h1>{times !== "" ? JSON.stringify(times.seconds.food/60) + " Mins" : "Loading"}</h1>
+            <h1 className='text-[1.75em] mb-2'>Food</h1>
+            <h1 className='text-[1.5em]'>{JSON.stringify(times.seconds.food/60) + " Mins"}</h1>
           </div>
           <div>
-            <h1 className='text-[1.2em] mb-2'>Drinks</h1>
-            <h1>{times !== "" ? JSON.stringify(times.seconds.drinks/60) + " Mins" : "Loading"}</h1>
+            <h1 className='text-[1.75em] mb-2'>Drinks</h1>
+            <h1 className='text-[1.5em]'>{JSON.stringify(times.seconds.drinks/60) + " Mins"}</h1>
           </div>
         </div>
 
